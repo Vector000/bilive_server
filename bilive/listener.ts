b0b13f971534c34ae1e67fee9d73e55eafe401e0
Vector000 <33326900+Vector000@users.noreply.github.com>
2019-03-18 07:19:41 +0800

    fix: 多分区调整
    多分区是个好东西 为啥某大佬就没写refresh

--- bilive/listener.ts
+++ bilive/listener.ts
@@ -94,7 +94,7 @@ class Listener extends EventEmitter {
    */
   public Start() {
     this.updateAreaRoom()
-    setInterval(() => this.updateAreaRoom(), 60 * 60 * 1000)
+    setInterval(() => this.updateAreaRoom(), 10 * 60 * 1000)
     this._RoomListener = new RoomListener()
     this._RoomListener
       .on('smallTV', (raffleMessage: raffleMessage) => this._RaffleHandler(raffleMessage))
@@ -200,7 +200,7 @@ class Listener extends EventEmitter {
       // 获取房间列表
       getAllList.body.data.module_list.forEach(modules => {
         if (modules.module_info.type !== 2 && modules.list.length > 2) {
-          for (let i = 0; i < modules.list.length; i++) roomIDs.add((<getAllListDataRoomList>modules.list[i]).roomid)
+          for (let i = 0; i < 3; i++) roomIDs.add((<getAllListDataRoomList>modules.list[i]).roomid)
         }
       })
       // 添加房间
